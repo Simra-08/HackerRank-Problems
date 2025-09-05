6c05596e09ec571ae35ac4674e2569ea367876a6
Simra Shahid <simrashahid7@gmail.com>
2025-09-05 19:00:09 +0530

    loop hackerrank

---
+++ loop hackerrank.py
@@ -0,0 +1,8 @@
+# Hackerrank Problem : Loop
+# Description : To print out the sqaure of the integer ranging from 0 to n 
+
+if __name__ == '__main__':
+    n = int(input())
+for i in range (0,n):
+    print (i*i)
+    
