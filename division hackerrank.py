5857e6af640989f9b0ca53835c0d82d4a7e232ee
Simra Shahid <simrashahid7@gmail.com>
2025-09-05 18:36:25 +0530

    division hackerrank

---
+++ division hackerrank.py
@@ -0,0 +1,9 @@
+# Hackerrank Problem : Division 
+# Description : To print the integer and float division of two integers 
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+print (a//b)
+print (a/b) 
+
