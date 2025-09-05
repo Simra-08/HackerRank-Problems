7adf3095f85ed3b558acb61edd138cef0d39edfa
Simra Shahid <simrashahid7@gmail.com>
2025-09-05 18:29:47 +0530

    hackerrank arithmetic

--- arithmetic hackerrank.py
+++ arithmetic hackerrank.py
@@ -1,3 +1,6 @@
+# Hackerrank Problem : Arithmetic Operations 
+# Description : To print out the result of addition, subtraction, and multiplication of two numbers
+
 if __name__ == '__main__':
     a = int(input())
     b = int(input())
