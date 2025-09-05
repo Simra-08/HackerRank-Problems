031828798648d85d8009bcf0354784aa1c2a4cc8
Simra Shahid <simrashahid7@gmail.com>
2025-09-05 19:17:26 +0530

    print a function hackerrank

---
+++ print function hackerrank.py
@@ -0,0 +1,8 @@
+# Hackerrank Problem : Print a Function
+# Description : To print the integers from 1 to n without spaces
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range (1,n+1):
+        print (i,end="")
+        
