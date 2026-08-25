4ca027df1a7090f73e9b5197e1e59d8425c52bf0
Simra Shahid <simrashahid7@gmail.com>
2026-08-25 13:06:50 +0530

    list comprehension hackerrank

--- Python Basics/list comprehension hackerank.py
+++ Python Basics/list comprehension hackerank.py
@@ -5,12 +5,10 @@ if __name__ == '__main__':
     y = int(input())
     z = int(input())
     n = int(input())
-
-a=[]
-for i in range (x+1):
-    for j in range (y+1):
-        for k in range (z+1):
-            if i+j+k != n:
-                a.append([i,j,k])
-                
-print(a)            
+    
+    numbers = [x,y,z]
+    elements = [[i,j,k] for i in range(0,x+1) for j in range(y+1) for k
+    in range(0,z+1) if i+j+k!=n]
+     
+    print(elements)
+    
