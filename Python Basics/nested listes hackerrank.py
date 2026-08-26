a683793ecdec33db5b0f1c28148ebf7b96f217fc
Simra Shahid <simrashahid7@gmail.com>
2026-08-26 13:51:00 +0530

    nested lists hackerrank

--- Python Basics/nested listes hackerrank.py
+++ Python Basics/nested listes hackerrank.py
@@ -2,17 +2,28 @@
 # Description : To print out the name alphabetically with the second lowest score
 
 if __name__ == '__main__':
-    records = []
+    students = []
+    scores = []
     for _ in range(int(input())):
         name = input()
         score = float(input())
-        records.append([name,score])
+        
+        students.append([name,score])
         
         
-    scores = [record[1] for record in records]
-    first_lowest = min(scores)
-    score2= [x for x in scores if x!= first_lowest]
-    second_lowest= min(score2)
-    names = [record[0] for record in records if record[1] == second_lowest]
-    for name in sorted(names):
-        print(name)
+    for i in students:
+        scores.append(i[1])
+        my_set = sorted(set(scores))
+        my_list = list(my_set)
+            
+    second_lowest = my_list[1]
+    
+    names = []
+    for i in students:
+        if i[1] == second_lowest:
+            names.append(i[0])
+    names.sort()
+    
+    for name in names:
+        print(name)        
+            
