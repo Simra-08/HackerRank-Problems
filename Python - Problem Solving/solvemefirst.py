7e54d932e883c0daddc605a31bd4e188716eb931
Simra Shahid <simrashahid7@gmail.com>
2026-09-19 17:24:20 +0530

    solvemefirst hackerrank

---
+++ Python - Problem Solving/solvemefirst.py
@@ -0,0 +1,11 @@
+# Hackerrank Problem : Solve Me First
+# Description : To return the sum of values
+
+def solveMeFirst(a,b):
+	return a + b
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
